ee36bb286b6a79a74a037048dd5a15d33929eb6e
Lance Douglas <ldouglastech@gmail.com>
2016-03-22 10:32:13 -0400

    Final touches.

--- src/main.kt
+++ src/main.kt
@@ -1,60 +1,78 @@
 fun main(args: Array<String>) {
-    var greet = "Hello there. What would you like to do? \nAddition? \nSubtraction \nMultiplication \nDivision"
-    var greetx = "Hello again. Have you decided on what you'd like to do? \nAddition? \nSubtraction \nMultiplication \nDivision"
-    println(greet)
+    var greet = "Hello there. What would you like to do? \n"
+    var greetx = "Hello again, \nhave you decided on what you'd like to do? \n"
+    var roundx = "Addition \nSubtraction \nMultiplication \nDivision"
+    println(greet + roundx)
 
     var myScn = readLine()
-fun readLine() = double?
-    when (myScn) {
-        "Addition","+" -> {
-            println("Let's do some addition!")
-            println("\nFirst Number?")
-            var plus0_raw = readLine()
-            println("Second number?")
-            var plus1_raw = readLine()
-            val plus0 = plus0_raw.toLong()
-            val plus1 = plus1_raw.toLong()
-            val addanswer = plus0 + plus1
-            println(addanswer)
-        }
-        "Subtraction" -> {
-            println("\nFirst Number?")
-            var ScnMinus = readLine()
-            println("Second number?")
-            var ScnMinus2 = readLine()
-            var subanswer = ScnMinus + ScnMinus2
-            println(subanswer)
-        }
-        "Division" -> {
-            println("Let's do some division.")
-            println("\nFirst Number?")
-            var ScnDiv = readLine()
-            println("Second number?")
-            var ScnDiv2 = readLine()
-            var divanswer = ScnDiv + ScnDiv2
-            println(divanswer)
-        }
-        "Multiplication" -> {
-            println("Let's multiply ;)")
-            println("\nFirst Number?")
-            var ScnMult = readLine()
-            println("Second number?")
-            var ScnMult2 = readLine()
-            var multanswer = ScnMult + ScnMult2
-            println(multanswer)
-        }
-        else -> {
-            println("Not an option")
-            println("Would you like to try again?")
-                var no = readLine()
-                if (no == "no") {
+    loop@ while(true) {
+        when (myScn) {
+            "Addition", "+" -> {
+                println("Let's do some addition!")
+                println("\nFirst Number?")
+                var ScnAdd1 = readLine()
+                println("Second number?")
+                var ScnAdd2 = readLine()
+                var plus0 = ScnAdd1!!.toDouble()
+                var plus1 = ScnAdd2!!.toDouble()
+                var addanswer = plus0 + plus1
+                println(addanswer)
+                break@loop
+
+            }
+            "Subtraction" -> {
+                println("\nFirst Number?")
+                var ScnMinus = readLine()
+                println("Second number?")
+                var ScnMinus2 = readLine()
+                var min0 = ScnMinus!!.toDouble()
+                var min1 = ScnMinus2!!.toDouble()
+                var subanswer = min0 - min1
+                println(subanswer)
+                break@loop
+            }
+            "Division" -> {
+                println("Let's do some division.")
+                println("\nFirst Number?")
+                var ScnDiv = readLine()
+                println("Second number?")
+                var ScnDiv2 = readLine()
+                var div0 = ScnDiv!!.toDouble()
+                var div1 = ScnDiv2!!.toDouble()
+                var divanswer = div0 / div1
+                println(divanswer)
+                break@loop
+            }
+            "Multiplication" -> {
+                println("Let's \"multiply\" ;)")
+                println("\nFirst Number?")
+                var ScnMult = readLine()
+                println("Second number?")
+                var ScnMult2 = readLine()
+                var mult0 = ScnMult!!.toDouble()
+                var mult1 = ScnMult2!!.toDouble()
+                var multanswer = mult0 * mult1
+                println(multanswer)
+                break@loop
+            }
+            else -> {
+                println("Not an option")
+                println("Would you like to try again?")
+                var yes = readLine()
+                if (yes == "no") {
                     println("\nHave a nice day.")
-                    }
-            else {
+                    return
+                } else {
                     println(greetx)
+                    break@loop
                 }
+            }
         }
     }
-
+    println("Would you like to do another?")
+    var yesno = readLine()
+    if (yesno == "Yes" || yesno == "Yeah")
+        println(roundx)
+    else
+        println("Have a nice day.")
 }
-// Education. All of the education.
